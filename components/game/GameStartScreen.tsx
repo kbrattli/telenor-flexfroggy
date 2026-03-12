a931fae755027cb56a11d9e3a9c80409a7c97f44
kbrattli <kbrattli@live.no>
2026-03-12 12:12:02 +0100

    større

--- components/game/GameStartScreen.tsx
+++ components/game/GameStartScreen.tsx
@@ -67,7 +67,7 @@ export default function GameStartScreen({
                     className="mx-auto mb-6 h-auto w-full max-w-xs"
                     priority
                   />
-                  <h1 className="mb-4 text-4xl font-bold text-telenor-dark-blue">
+                  <h1 className="mb-4 text-6xl font-bold text-telenor-dark-blue">
                     VINN 5000 KR
                   </h1>
                   <AnimatedHandwriting
